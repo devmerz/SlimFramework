78a1d3cdb145fd6f500bd6e6d76fb8950e5e7620
Elmer Zapana <ezapana@kisimira.com>
2014-11-16 15:45:15 -0400

    Slim funcionando sin conexion a BD

--- rest.php
+++ rest.php
@@ -1,3 +1,31 @@
 <?php 
-    echo 'HOLA ELMER';
+    require 'Slim/Slim.php';
+    \Slim\Slim::registerAutoloader();
+    $app = new \Slim\Slim();
+    
+    $app->config(array(
+        'templates.path' => 'vistas',
+    ));
+    $app->contentType('text/html; charset=utf-8');
+
+    
+    // REST 
+    $app->get("/", function () {
+        //Retorname algun dato
+        echo "<h1>Hola elmer</h1>";
+    });
+    
+    $app->post('/books', function () {
+        //Create book
+        echo 'Se creara el librito ';
+    });
+    $app->put('/book/:id', function ($id) {
+        echo 'Se Actualizara el registro con id : '.$id;
+    });
+    $app->delete('/bookDelete/:id', function ($id) {
+        echo 'S eliminara el registro con id :'.$id;
+    });
+
+    $app->run();
+    
  ?>
